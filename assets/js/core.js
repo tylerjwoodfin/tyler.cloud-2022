58c087e220df4e8147e883c465cc36fe1cd4370c
Tyler Woodfin <tyler@tylerwoodfin.com>
2022-02-06 01:33:04 -0600

    Added 'More' menu

--- assets/js/core.js
+++ assets/js/core.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
   $("#reachout").hide();
   $("#latest").removeClass("hidden");
   $("#latest").hide();
+  $(`#more`).removeClass("hidden");
+  $(`#more`).hide();
   $(`.load-spin-send`).removeClass("hidden");
   $(`.load-spin-send`).hide();
   $(`.about`).removeClass("hidden");
@@ -15,29 +17,29 @@ $(document).ready(function () {
 });
 
 function getLatest() {
+  $("#latest").prepend(
+    `<a href="https://www.github.com/tylerjwoodfin" class="sub" target=_new>GitHub...</a>`
+  );
   $.getJSON("https://api.github.com/users/tylerjwoodfin/repos", {
     format: "json",
   })
     .done(function (data) {
-      $("#latest").empty();
-      $(_.reverse(_.sortBy(data, "pushed_at"))).each(function (i, item) {
-        $("#latest").append(
+      $(_.sortBy(data, "pushed_at")).each(function (i, item) {
+        $("#latest").prepend(
           `<a href="${item.html_url}" class="sub" target=_new>${item.name}</a>\n`
         );
         return i < 6;
       });
     })
-    .then(function () {
-      $("#latest").append(
-        `<a href="https://www.github.com/tylerjwoodfin" class="sub" target=_new>More on GitHub...</a>`
-      );
+    .always(function () {
+      $("#load-spin-latest").hide();
     });
 }
 
 function toggleButton(name) {
   $(`#${name}`).toggle();
 
-  if (name === "latest" && $("#latest")[0].innerHTML.split("<a").length < 2) {
+  if (name === "latest" && $("#latest")[0].innerHTML.split("<a").length < 3) {
     getLatest();
   }
   if (name === "reachout") {
@@ -50,7 +52,7 @@ function toggleButton(name) {
     $(`#button-${name}`)[0].style.textDecoration = `underline`;
     $(`[id^=button]`).not(`#button-${name}`).hide();
     $(`[id^=arrow]`).not(`#arrow-${name}`).hide();
-    $(`[id^=arrow]`).show();
+    $(`[id^=arrow-${name}]`).show();
   } else {
     //hide
     $(`textarea`).val("");
