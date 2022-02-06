b095836b90d458ebcafa28f61b56a912e53e8aee
Tyler Woodfin <tyler@tylerwoodfin.com>
2022-02-05 19:48:42 -0600

    Added About section

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,81 +1,83 @@
 // initialize
 $(document).ready(function () {
-    $("#reachout").removeClass("hidden");
-    $("#latest").removeClass("hidden");
-    $(`.load-spin-send`).removeClass("hidden");
-    $("#reachout").hide();
-    $("#latest").hide();
-    $(`.load-spin-send`).hide();
+  $("#reachout").removeClass("hidden");
+  $("#reachout").hide();
+  $("#latest").removeClass("hidden");
+  $("#latest").hide();
+  $(`.load-spin-send`).removeClass("hidden");
+  $(`.load-spin-send`).hide();
+  $(`.about`).removeClass("hidden");
+  $(`.about`).hide();
 });
 
 function getLatest() {
-    $.getJSON("https://api.github.com/users/tylerjwoodfin/repos", {
-        format: "json",
+  $.getJSON("https://api.github.com/users/tylerjwoodfin/repos", {
+    format: "json",
+  })
+    .done(function (data) {
+      $("#latest").empty();
+      $(_.reverse(_.sortBy(data, "pushed_at"))).each(function (i, item) {
+        $("#latest").append(
+          `<a href="${item.html_url}" class="sub" target=_new>${item.name}</a>\n`
+        );
+        return i < 6;
+      });
     })
-        .done(function (data) {
-            $("#latest").empty();
-            $(_.reverse(_.sortBy(data, "pushed_at"))).each(function (i, item) {
-                $("#latest").append(
-                    `<a href="${item.html_url}" class="sub" target=_new>${item.name}</a>\n`
-                );
-                return i < 6;
-            });
-        })
-        .then(function () {
-            $("#latest").append(
-                `<a href="https://www.github.com/tylerjwoodfin" class="sub" target=_new>More...</a>`
-            );
-        });
+    .then(function () {
+      $("#latest").append(
+        `<a href="https://www.github.com/tylerjwoodfin" class="sub" target=_new>More on GitHub...</a>`
+      );
+    });
 }
 
 function toggleButton(name) {
-    $(`#${name}`).toggle();
+  $(`#${name}`).toggle();
 
-    if (name === "latest" && $("#latest")[0].innerHTML.split("<a").length < 2) {
-        getLatest();
-    }
-    if (name === "reachout") {
-        $(`.button-reachout-send`).show();
-        $(`.textarea-${name}`)[0].focus();
-    }
+  if (name === "latest" && $("#latest")[0].innerHTML.split("<a").length < 2) {
+    getLatest();
+  }
+  if (name === "reachout") {
+    $(`.button-reachout-send`).show();
+    $(`.textarea-${name}`)[0].focus();
+  }
 
-    // show
-    if ($(`#${name}`)[0].style.display !== `none`) {
-        $(`#button-${name}`)[0].style.textDecoration = `underline`;
-        $(`[id^=button]`).not(`#button-${name}`).hide();
-        $(`[id^=arrow]`).not(`#arrow-${name}`).hide();
-        $(`[id^=arrow]`).show();
-    } else {
-        //hide
-        $(`textarea`).val("");
-        $(`#button-${name}`)[0].style.textDecoration = ``;
-        $(`[id^=arrow]`).hide();
-        $(`[id^=button]`).show();
-        $("#reachout-send-results").html("");
-    }
+  // show
+  if ($(`#${name}`)[0].style.display !== `none`) {
+    $(`#button-${name}`)[0].style.textDecoration = `underline`;
+    $(`[id^=button]`).not(`#button-${name}`).hide();
+    $(`[id^=arrow]`).not(`#arrow-${name}`).hide();
+    $(`[id^=arrow]`).show();
+  } else {
+    //hide
+    $(`textarea`).val("");
+    $(`#button-${name}`)[0].style.textDecoration = ``;
+    $(`[id^=arrow]`).hide();
+    $(`[id^=button]`).show();
+    $("#reachout-send-results").html("");
+  }
 }
 
 function handleSend() {
-    $(`.button-reachout-send`).hide();
-    $(`.load-spin-send`).show();
+  $(`.button-reachout-send`).hide();
+  $(`.load-spin-send`).show();
 
-    $.get(
-        "/assets/php/feedback.php?subject=Website&message=" +
-            $("#textarea-reachout")[0].value,
-        function (data) {
-            $("#reachout-send-results").text(
-                "Thank you! Your message has been sent."
-            );
-            $("#textarea-reachout")[0].value = "";
-            $("#textarea-reachout").hide();
-            $(`.load-spin-send`).hide();
-        },
-        "text"
-    ).fail(function () {
-        $("#reachout-send-results").html(
-            "There was a problem sending your message. Please reach out through <a href='https://linkedin.com/in/tylerjwoodfin' target='_new'>LinkedIn</a>."
-        );
-        $(`.button-reachout-send`).show();
-        $(`.load-spin-send`).hide();
-    });
+  $.get(
+    "/assets/php/feedback.php?subject=Website&message=" +
+      $("#textarea-reachout")[0].value,
+    function (data) {
+      $("#reachout-send-results").text(
+        "Thank you! Your message has been sent."
+      );
+      $("#textarea-reachout")[0].value = "";
+      $("#textarea-reachout").hide();
+      $(`.load-spin-send`).hide();
+    },
+    "text"
+  ).fail(function () {
+    $("#reachout-send-results").html(
+      "There was a problem sending your message. Please reach out through <a href='https://linkedin.com/in/tylerjwoodfin' target='_new'>LinkedIn</a>."
+    );
+    $(`.button-reachout-send`).show();
+    $(`.load-spin-send`).hide();
+  });
 }
